1a904a4471fc17f42d99f59f3645c6983773185a
Valdelamar <estivengutierrez1997@gmail.com>
2025-08-01 07:53:27 -0500

    Cree la app (programas) y configuré sus vistas

---
+++ Sena_App/programas/views.py
@@ -0,0 +1,13 @@
+from django.http import HttpResponse
+from django.template import loader
+from .models import Programa
+# Create your views here.
+
+def programasv(request):
+    lista_programas = Programa.objects.all().order_by('nombre')
+    template = loader.get_template('lista_programas.html')
+    context = {
+    'lista_programas': lista_programas,
+    'total_programas': lista_programas.count(),
+    }
+    return HttpResponse(template.render(context, request))
